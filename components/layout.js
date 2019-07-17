caa61d9779857af2e76b3cbd0fe7b5f1e17015d0
Siwakorn Sittilerdpaisal <s.sittilerdpaisal@gmail.com>
2019-07-17 22:40:31 +0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,16 @@
 import Header from '../components/header';
 
+// มี Styled Components ใน Project แล้ว ผมว่าเรียกมันมาใช้แทน LayoutStyle ดีกว่า
+// const LayoutContainer = styled.div``
+// ผมชอบกรุ๊ปให้เป็น Folder มากกว่าเช่น
+// Components
+// |_Layout
+//    |_ index.js
+//    |_ container.js 
+// จะได้แยก Styled ออก
+// ปกติพวก Header ควรมาใช้แบบนี้มากกว่านะ 
+// https://nextjs.org/docs#custom-app
+
 const LayoutStyle = {
   margin: 20,
   padding: 20,
