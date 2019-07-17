9a4ff50baaf59c6e8ab0b9ba9319c580eaa7b7fa
Siwakorn Sittilerdpaisal <s.sittilerdpaisal@gmail.com>
2019-07-17 23:29:05 +0700

    Update api.js

--- api/api.js
+++ api/api.js
@@ -4,6 +4,10 @@ const api = axios.create({
     baseURL: 'https://68adda0f-b2f3-446b-b72c-45e5823ef4e0.mock.pstmn.io/api',
 });
 
+// ปกติมันน่าจะตั้งว่า /products ควรใช้ Const แทนด้วย
+// พอผมเห็นเขียนมาแบบนี้ผมจะเริ่มไม่ไว้ใจละว่าตอนแก้ผมแก้หมดจริงเปล่า
+// https://restfulapi.net/resource-naming/
+
 export const getProducts = () => {
    return api.get('/product/')
 } 
@@ -12,5 +16,7 @@ export const getProduct = (id) => {
 } 
 export const updateProductData = (id, data) => {
     // PUT METHOD Should work properly if you have API
+    // อันนี้ควรตัวผมชอบใช้ Template Literal มากกว่า ผมว่ามันอ่านเป็นธรรมชาติกว่า
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
    return api.put('/product/'+id, data, {headers: {'Content-Type': 'application/json'}})
 }
