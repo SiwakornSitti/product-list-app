fe1ef6a3e793096d3bb3bfba55485735ce816df0
Siwakorn Sittilerdpaisal <s.sittilerdpaisal@gmail.com>
2019-07-17 22:48:35 +0700

    Update product-detail.js

--- components/product-detail.js
+++ components/product-detail.js
@@ -1,8 +1,16 @@
 import { ProductImg } from '../styled-css/product'
 
+// 1 File ควรมีแค่ Component เดียวนะ
+// อันนี้มันควรเป็น ImageList รึเปล่าเพราะสุดท้ายมันคือ Render image หลาย ๆ Elements
+
 const Image = ({ images }) => {
+    // ควรแสดงข้อความอะไรสักอย่างให้ User เห็นว่า ไม่มีรูปภาพประกอบ
     if (!images) return (<div/>)
     return (
+                         
+        // อย่าใช้ Index ในการแก้ไขปัญหา Key มันทำให้การเปรียบเทียบ Virtual DOM มีปัญหา
+        // https://reactjs.org/docs/lists-and-keys.html
+        // https://medium.com/@robinpokorny/index-as-a-key-is-an-anti-pattern-e0349aece318
         images.map((image, i) => {
             return (
                 <div key={i}>
@@ -17,6 +25,7 @@ const Image = ({ images }) => {
 const ProductDetail = ({ product }) => {
     if (!product) return (<div/>)
     return (
+        // ใช้ Fragment แทน        
         <div>
             <h1>{product.name}</h1>
             <span>{product.number}</span>
