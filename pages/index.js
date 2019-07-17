2c53340f8d3765ee35ba2af5a3de1bc3b4837d4e
Siwakorn Sittilerdpaisal <s.sittilerdpaisal@gmail.com>
2019-07-17 23:11:52 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 import ProductList from '../components/product-list'
 import { fetchProducts } from '../actions';
 
-// ใช้ Functionall Component แทน
+// ใช้ Functional Component แทน
 class Index extends Component {
     componentDidMount () {
         this.props.fetchProducts()
